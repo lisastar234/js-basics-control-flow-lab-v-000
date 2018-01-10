931f2b664074f8dda13e2dbf2fbcea090819a179
Lisa Starzyk <lisa.starzyk@gmail.com>
2018-01-10 01:09:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet)
+  if (feet <=400) {
+    return 'This one is on me! ' ;
+  } 
+ else if (feet >2000) {
+  return 'I will gladly take your thirty bucks.' ;
+  }
+ else (feet > 2500) {
+    return 'No go.';
+ };
+  
+ 
+const ternaryCheckCity = city === 'NYC' ? 'Ok, sounds good.' : 'No, go. ';
+const NYC;
+
